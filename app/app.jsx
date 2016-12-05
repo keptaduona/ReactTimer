61aff6e8f132cac4b2ab298a5e8e95a671407125
Matas Kinderis <mataskind@gmail.com>
2016-12-05 13:18:43 +0200

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,19 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const {Route, Router, IndexRoute, hashHistory} = require('react-router');
+const Main = require('Main');
+
+// Load foundation using css and style loader
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+    </Route>
+  </Router>,
+  document.getElementById('app')
+);
